dc63653cd22af24dc4cf2fb75e7ce8bb7a686a73
jerdasi <jeremiadansil@gmail.com>
2022-03-29 15:04:44 +0700

    ancar-ancar lagi guys

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,3 +5,27 @@ var msnry = new Masonry(grid, {
     gutter: '.gutter-sizer',
     percentPosition: true
 })
+
+document.querySelectorAll('.grid-item').forEach(elemen => {
+    let description = elemen.firstElementChild
+    elemen.addEventListener('mouseenter', () => {
+        description.classList.remove('border-4', 'border-emas-keraton')
+        description.classList.add('shadow-mia-style')
+        elemen.classList.add('scale-110')
+        elemen.lastElementChild.classList.remove('hidden')
+            // elemen.add('scale-110')
+            // description.classList.remove('hidden')
+            // description.classList.add('rounded-b-lg')
+    })
+    elemen.addEventListener('mouseleave', () => {
+        description.classList.add('border-4', 'border-emas-keraton')
+        description.classList.remove('shadow-mia-style')
+        elemen.classList.remove('scale-110')
+        elemen.lastElementChild.classList.add('hidden')
+            // elemen.remove('scale-110')
+            // elemen.lastElementChild.classList.add('hidden')
+            // description.classList.remove('rounded-b-lg')
+    })
+})
+
+// border - 4 hover: border - none hover: scale - 110 hover: shadow - mia - style hover: rounded - lg border - emas - keraton rounded - lg
